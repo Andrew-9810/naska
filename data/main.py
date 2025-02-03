9e6882a5abd38edc2c928bfa3c8e2ea855d05f66
Golovanov Andrey <andrew-9810@yandex.ru>
2025-02-03 18:28:44 +0300

    Вкладки создал

--- data/main.py
+++ data/main.py
@@ -1,7 +1,7 @@
 from PySide6 import QtGui, QtWidgets
 from data.output_data import ReadeFile
 
-CMNT_CHARACTERS = 20  # Колличесто вимволов в комментари
+CMNT_CHARACTERS = 20  # Колличесто символов в комментари
 # Заголовки которые берем из json
 list_header = ['name', 'surname', 'card_number', 'bib', 'year', 'comment']
 
@@ -9,8 +9,17 @@ class MyWindow(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
 
-        tv = QtWidgets.QTableView(parent=self.window())
-        sti = QtGui.QStandardItemModel(parent=self.window())
+        tab = QtWidgets.QTabWidget()
+        tv = QtWidgets.QTableView()
+        sti = QtGui.QStandardItemModel()
+        # Вкладки
+        tab.addTab(tv, "&Person")
+        tab.addTab(QtWidgets.QLabel("Группы"), "&Group")
+        tab.addTab(QtWidgets.QLabel("Команды"), "&Teams")
+        tab.setCurrentIndex(0)
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addWidget(tab)
+        self.setLayout(vbox)
 
         self.data_list = ReadeFile('20241020.json')
         self.data_list.read_file()
@@ -28,7 +37,7 @@ class MyWindow(QtWidgets.QWidget):
                 name, surname, card_number, bib,
                 year, group_id, organization_id,
             ]
-
+            # Разбиваю каоментарий на 20 символов, WO дает 20
             comment = list(i['comment'])
             for index in range(CMNT_CHARACTERS):
                 try:
@@ -44,8 +53,9 @@ class MyWindow(QtWidgets.QWidget):
         )
         sti.itemChanged.connect(self.cell_changed)
         tv.setModel(sti)
-        tv.setColumnWidth(0, 50)
-        tv.setColumnWidth(2, 180)
+        # Ширина заголовков
+        tv.setColumnWidth(0, 60)
+        tv.setColumnWidth(2, 80)
         for i in range(7, 28, 1):
             tv.setColumnWidth(i, 1)
         tv.resize(1200, 800)
@@ -55,10 +65,12 @@ class MyWindow(QtWidgets.QWidget):
         self.data_list.data['races'][0]['persons'][item.row()][list_header[item.column()]] = item.text()
         self.data_list.writer_file()
 
+
+
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
-    window.setWindowTitle("QStandardltemModel")
+    window.setWindowTitle("Сбор стартового взноса")
     window.show()
     sys.exit(app.exec())
